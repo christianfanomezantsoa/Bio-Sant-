11edb8af45cb367fc9b42de8f50f307af865b9c0
christian fanomezantsoa <zarasoa01@gmail.com>
2020-11-15 21:31:57 +0100

    2 em modification

---
+++ js/java.js
@@ -0,0 +1,11 @@
+
+const voir_plus=document.querySelector(".voir_plus");
+const article_footer=document.querySelector(".article_footer");
+
+
+
+voir_plus.addEventListener("click", () => {
+	voir_plus.classList.toggle("hover_voir_plus");
+	// article_footer.classList.toggle("hover_x");
+
+})
