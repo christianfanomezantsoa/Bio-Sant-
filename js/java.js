624cfa205ae9f113bca366657d92b4fff8779430
christian fanomezantsoa <zarasoa01@gmail.com>
2020-11-16 17:19:01 +0100

    Reprise sur JS

--- js/java.js
+++ js/java.js
@@ -1,11 +1,13 @@
 
-const voir_plus=document.querySelector(".voir_plus");
-const article_footer=document.querySelector(".article_footer");
 
 
 
-voir_plus.addEventListener("click", () => {
-	voir_plus.classList.toggle("hover_voir_plus");
-	// article_footer.classList.toggle("hover_x");
 
+
+var article_footer=document.querySelector(".article_footer div");
+var voir_plus=document.querySelector(".voir_plus");
+
+article_footer.addEventListener("click", () => {
+voir_plus.classList.toggle("hover_voir_plus");
+article_footer.classList.toggle("hover_x");
 })
