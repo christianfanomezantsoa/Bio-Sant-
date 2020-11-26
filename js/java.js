895463c47e2ca8d38ca28996e108314d2c4b47ef
christian fanomezantsoa <zarasoa01@gmail.com>
2020-11-26 21:35:26 +0100

    bootstrap

--- js/java.js
+++ js/java.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 var article_footer=document.querySelector(".article_footer div");
 var voir_plus=document.querySelector(".voir_plus");
 
@@ -11,3 +7,4 @@ article_footer.addEventListener("click", () => {
 voir_plus.classList.toggle("hover_voir_plus");
 article_footer.classList.toggle("hover_x");
 })
+
